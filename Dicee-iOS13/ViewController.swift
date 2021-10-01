e406bc6a8bc2307c30e9f504494afd75e204919e
OmRajpurkar <om.sam1999@gmail.com>
2021-10-01 19:35:26 +0530

    My learning changes

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,19 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+        
+        diceImageView1.image = UIImage(named: diceArray.randomElement()!)
+        diceImageView2.image = UIImage(named: diceArray.randomElement()!)
+        
     }
-
-
+    
 }
 
