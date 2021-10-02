45bbe443712b908d9821ec1b7abda851be376cab
OmRajpurkar <om.sam1999@gmail.com>
2021-10-02 12:29:27 +0530

    Added the Shake Functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -5,14 +5,29 @@ class ViewController: UIViewController {
     // IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeDiceFace()
+    }
+    
+    //MARK: - Roll Button Methods
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        let diceArray = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
-        
+        changeDiceFace()
+    }
+    
+    func changeDiceFace(){
         diceImageView1.image = UIImage(named: diceArray.randomElement()!)
         diceImageView2.image = UIImage(named: diceArray.randomElement()!)
-        
+    }
+    
+    // Shake Gesture
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        changeDiceFace()
     }
     
 }
